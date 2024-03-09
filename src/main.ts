e29804947a40063b9e3d8abdce9788a87f7f78ed
halinad <gaxin@list.ru>
2024-03-10 00:36:47 +0300

    feat: add drafts for user

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(4000);
+  console.log(`Server started on ${await app.getUrl()}`);
 }
 bootstrap();
