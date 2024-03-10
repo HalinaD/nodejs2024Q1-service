71bc7111b6d8f202776f09ca64b799777a50e4de
halinad <gaxin@list.ru>
2024-03-10 22:11:24 +0300

    feat: add artist

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ArtistModule } from './artist/entities/artist.module';
 import { DbModule } from './db/db.module';
 import { TrackModule } from './track/entities/track.module';
 import { UserModule } from './user/entities/user.module';
 
 @Module({
-  imports: [DbModule, UserModule, TrackModule],
+  imports: [DbModule, UserModule, TrackModule, ArtistModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/db/db.service.ts
+++ src/db/db.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { ArtistDto } from 'src/artist/dto/artist.dto';
 import { UserDto } from 'src/user/dto/user.dto';
 
 @Injectable()
 export class DbService {
   users: UserDto[] = [];
   tracks: TrackDto[] = [];
+  artists: ArtistDto[] = [];
 }
